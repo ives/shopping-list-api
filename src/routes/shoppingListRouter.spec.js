8511c2d3116a6477a99640bdbdd3ae73539ad32d
Ives <ives@Ivans-Mac-mini.home>
2020-05-15 14:47:55 +0100

    Travis setup

---
+++ src/routes/shoppingListRouter.spec.js
@@ -0,0 +1,19 @@
+//const shoppingListRouter = require('./shoppingListRouter')();
+
+const assert = require('assert');
+const should = require('chai').should();
+
+// should can only be included in one test
+
+describe('basic test', () => {
+  it('should deal with Objects', () => {
+    const obj = { name: 'Ives', gender: 'male' };
+    const objB = { name: 'Ives', gender: 'male' };
+
+    obj.should.have.property('name');
+    obj.should.have.property('name').equal('Ives');
+
+    // Compare Obj - use deep, else checks if instance of same Obj
+    obj.should.deep.equal(objB);
+  });
+});
